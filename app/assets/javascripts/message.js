28466208bd8efebafa44694ae41a7d23df8f27d8
shoh8190 <SHO@hoshinoshou-no-MacBook-Air.local>
2018-04-09 21:38:23 +0900

    指摘をいただいた点のfix

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,11 +1,18 @@
 $(function(){
+  // $('.main__form__message').keypress(function(e){
+  //   if(e.witch && e.witch === 13 || e.keyCode && e.keyCode === 13){
+  //     id(!$(this).val().match(/\S/g)){
+  //       return false;
+  //     }
+  //   }
+  // });
   function buildHTML(message){
     if(message.image == null){
       var image = "";
     } else {
       var image = `${message.image}`;
     }
-    var html = `<div class="main__chat__post" id="${message.id}">
+    var html = `<div class="main__chat__post" data-message-id="${message.id}">
                   <div class="main__chat__post__name">
                     <h3> ${message.user_name} </h3>
                     <p> ${message.created_at} </p>
@@ -17,7 +24,18 @@ $(function(){
                </div>`
     return html;
   }
+
+  function preventSubmit(){
+    var content = document.getElementById(".main__form__message").value;
+    var image = document.getElementById(".main__form__image").value;
+
+    if (content === null || image == null) {
+        return false;
+    }
+  }
+
   $('#message_form').on('submit', function(e){
+    // preventSubmit();
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
@@ -30,10 +48,13 @@ $(function(){
       contentType: false
     })
     .done(function(data){
+      if(data.id != undefined ){
       var html = buildHTML(data);
-      $('.main__chat').append(html)
-      $('#message_form')[0].reset();
+      $('.main__chat').append(html);
+      $('.main__form__message').val('');
+      $('.main__form__image').val('');
       $('.main__chat').animate({scrollTop: $(".main__chat")[0].scrollHeight}, 'slow');
+    }
     })
     .fail(function(){
       alert('error');
@@ -44,20 +65,19 @@ $(function(){
   var interval = setInterval(update, 5000);
 
     function update(){
-    var id = $('.main__chat__post').last().attr('id');
+    var id = $('.main__chat__post').last().data('messageId');
     var link = window.location.pathname;
     if (link.match(/\/groups\/\d+\/messages/)){
   $.ajax({
       url: link,
       type: 'GET',
       data: {id: id},
-      dataType:'json'
+      dataType:'json',
   })
   .done(function(json){
     if (json.length !== 0){
     json.forEach(function(message){
-      var html = buildHTML(message);
-      $('.main__chat').append(html);
+      $('.main__chat').append(buildHTML(message));
     });
     }
     $('.main__chat').animate({scrollTop: $(".main__chat")[0].scrollHeight}, 'slow');
@@ -69,5 +89,3 @@ $(function(){
     clearInterval(interval);
   }}
 });
-
-
