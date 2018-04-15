59da483b7d114b68631e0e2ddec2955746a82153
shoh8190 <SHO@hoshinoshou-no-MacBook-Air.local>
2018-04-15 19:25:21 +0900

    fix

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,3 @@
-server '52.69.180.219', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
